02cb06c9a7b65e66352e5e43c7149484b4f85987
David Dudson <d.dudson@psenterprise.com>
2015-11-17 22:13:31 +1300

    Switch to port 5000 for heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+// Must be 5000 for heroku
+var server = app.listen(5000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
