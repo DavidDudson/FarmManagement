8c683968276bb8694a271461908c4764efc6159e
David Dudson <d.dudson@psenterprise.com>
2015-11-18 20:12:08 +1300

    Get Livereload of webpack and serverfiles

---
+++ src/server/server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var webpack = require('webpack');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+// 9000 works for deploy and develop
+var server = app.listen(9000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
+webpack({
+    entry: './src/client/webpack.js',
+    output: {path: '../dist/public', filename: 'bundle.js'},
+    module: {
+        loaders: [
+            {test: /\.css$/, loader: "style!css"},
+            {test: /\.(scss|sass)$/, loader: "style!sass"},
+            {test: /\.js$/, loader: "babel"},
+            {test: /\.html$/, loader: "html"},
+            {test: /\.(png|jpg|gif|bmp)$/, loader: "url?prefix=img/&limit=5000"},
+            {test: /\.(woff|woff2)(\?|$)/, loader: "url?limit=5000&minetype=application/font-woff"},
+            {test: /\.(eot|ttf|svg)(\?|$)/, loader: "file?prefix=font/"}
+        ]
+    },
+    resolve: {
+        modulesDirectories: ['node_modules']
+    }
+}, function (err, stats) {
+    console.log(err);
+    console.log(stats.toJson());
+});
