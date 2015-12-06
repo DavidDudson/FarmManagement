2e132f543d78f19ee8239b09d745ab96758d211d
David Dudson <d.dudson@psenterprise.com>
2015-12-06 21:56:29 +1300

    WIP do some cool stuff

--- src/client/app/components/table/table.js
+++ src/client/app/components/table/table.js
@@ -1,18 +1,31 @@
 require('./table.scss');
 
 class TableCtrl {
-    constructor() {
-        this.randomthing = test;
+    constructor(title, description) {
+        this.table = {};
+        this.title = title;
+        this.description = description;
+    }
+
+    calculate(value) {
+        if (_.isString(value) && value === "") {
+            return "";
+        } else if (_.isNumber(value)){
+            return value;
+        } else {
+            return "";
+        }
     }
 }
 
 class TableDirective {
     constructor() {
-        this.template = '<div>I\'m a directive!</div>';
+        this.template = require('./table.html');
         this.restrict = 'E';
+        this.replace = true;
     }
 }
 
 angular.module('app')
     .controller('TableCtrl', TableCtrl)
-    .directive('farm-table', TableDirective);
+    .directive('spreadsheet', () => new TableDirective);
