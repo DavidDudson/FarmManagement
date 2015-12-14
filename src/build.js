715b2b236e583512ee6041da37456c45efbcdfbc
David Dudson <d.dudson@psenterprise.com>
2015-12-14 18:44:35 +1300

    Seperate webpack conf and make webpack an npm run script

--- src/build.js
+++ src/build.js
@@ -1,10 +1,10 @@
 module.exports = () => {
 
-    var WebpackNotifierPlugin = require('webpack-notifier');
     var _ = require('lodash');
     var rm = require('rimraf');
     var path = require('path');
     var webpack = require('webpack');
+    var webpackConf = require('./webpack.conf.js');
     var homedir = path.resolve(__dirname + "/../");
 
 
@@ -14,49 +14,8 @@ module.exports = () => {
 
     console.log("Webpack Rebuilding...\n");
 
-    webpack({
-        entry: {
-            bundle: path.resolve(homedir, 'src/client/app/app.js'),
-        },
-        debug: true,
-        devtool: 'source-map',
-        output: {
-            path: path.resolve(homedir, 'dist/public'),
-            publicPath: '/',
-            filename: '[name].js'
-        },
-        module: {
-            loaders: [
-                {test: /\.css$/, loader: "style!css"},
-                {test: /\.(scss|sass)$/, loader: "style!css?sourceMap!resolve-url?sourceMap!sass?sourceMap"},
-                {test: /\.jsx?$/, loader: "babel", query: {presets: ['es2015']}},
-                {test: /\.html$/, loader: "html"},
-                {test: /\.(png|jpg|gif|bmp)$/, loader: "url?prefix=img/&limit=5000"},
-                {test: /\.(woff|woff2)(\?|$)/, loader: "url?limit=5000&minetype=application/font-woff&name=fonts/[name].[ext]"},
-                {test: /\.(eot|ttf|svg)(\?|$)/, loader: "file?prefix=font/&name=fonts/[name].[ext]"}
-            ]
-        },
-        sassLoader: {
-            includePaths: [
-                path.resolve(homedir, "node_modules/compass-mixins/lib")
-            ]
-        },
-        resolve: {
-            root : [homedir],
-            alias: {
-                util: "src/client/app/util",
-                app: "src/client/app",
-                client: "src/client",
-                angular_material_css: "angular-material/angular-material.min.css",
-                angular_data_table_css: "angular-material-data-table/dist/md-data-table.min.css",
-            },
-            extensions: ['', '.js', '.scss']
 
-        },
-        plugins: [
-            new WebpackNotifierPlugin()
-        ]
-    }, (err, stats) => {
+    webpack(webpackConf, (err, stats) => {
         if (err) console.warn(err);
 
         console.log("\nVendor modules: \n");
@@ -66,7 +25,7 @@ module.exports = () => {
         var vendor = stats.compilation.fileDependencies
             .filter(f => f.includes("node_modules") && !f.includes("loader"))
             .map(f => f.replace(path.join(homedir, "node_modules"), ""))
-            .map(f => f.match(/^\/(.[^\/]*)\//)[1])
+            .map(f => f.match(/^\/(.[^\/]*)\//)[1]);
 
         _.uniq(vendor)
             .forEach(f => console.log(" - " + f));
