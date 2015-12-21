fc0c5ce1d7734a60bb844147ff79df5c4199f60a
David Dudson <d.dudson@psenterprise.com>
2015-12-21 20:57:05 +1300

    Make Directive Visible

--- src/client/app/components/nav/nav.js
+++ src/client/app/components/nav/nav.js
@@ -1,5 +1,7 @@
 require('angular_carousel_css');
 
+require('./nav.scss')
+
 class NavCtrl {
     constructor() {
         this.nav = [{"title" : "Topic One", "icon" : "image.jpg", "description" : "This is a descriptive topic"},
@@ -11,12 +13,20 @@ class NavCtrl {
 
 class NavDirective {
     constructor() {
-        this.template = "<nav> <ul rn-carousel class='image'> <li>slide #1</li> <li>slide #2</li> <li>slide #3</li> </ul> </nav>";
+        this.template = `
+        <nav>
+            <ul rn-carousel rn-carousel-auto-slide>
+                <li>slide #1</li>
+                <li>slide #2</li>
+                <li>slide #3</li>
+            </ul>
+        </nav>`;
+
         this.restrict = 'E';
     }
 }
 
 angular.module('app')
     .controller('NavCtrl', NavCtrl)
-    .directive('navScroll', NavDirective);
+    .directive('navScroll', () => new NavDirective());
 
