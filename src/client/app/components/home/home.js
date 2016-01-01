69dd601022543448ce890f1d730e1019007b0539
Anthony Crowcroft <anthonycrowcroft@Anthonys-iMac.local>
2016-01-01 15:00:57 +1300

    Column layout

--- src/client/app/components/home/home.js
+++ src/client/app/components/home/home.js
@@ -1,9 +1,9 @@
 class HomeCtrl {
     constructor() {
-        // Infographics could be pairs of image/description
-        // And a nice hover over to show description with negative text
         this.infographics = [];
-        this.description = "Some Description"
+        this.description = `Welcome to FarmFINANZ!
+            Take your knowledge to the next level.The fundamentals in Farm Management are about getting the maths right.
+            We are here to assist you in every aspect of farm finance.We have a series of tutorials, quizzes and calculators that help you to get or refresh the knowledge you need to meet the challenges of farming today. We can help you to sharpen your maths skills in every field of farm management, ultimately to identify business opportunities and define future goals. Best of all, you’ll be learning from Massey professionals in this field, who developed the quizzes with you in mind.`;
     }
 }
 
@@ -21,6 +21,7 @@ angular.module('app')
             .state("home" , {
                 url:'/home',
                 template: require('./home.html'),
-                controller: HomeCtrl
+                controller: HomeCtrl,
+                controllerAs: 'home'
             })
     });
