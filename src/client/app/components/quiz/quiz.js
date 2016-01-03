102b4c63b12ce53d142207af54652d1bcbf36ca6
David Dudson <d.dudson@psenterprise.com>
2016-01-03 17:33:14 +1300

    Work on getting quizzes showing data

--- src/client/app/components/quiz/quiz.js
+++ src/client/app/components/quiz/quiz.js
@@ -2,16 +2,14 @@ require('./quiz.scss');
 
 class QuizCtrl {
     constructor($rootScope, $stateParams) {
-        console.log($stateParams)
         var topic = $rootScope.app.topics[$stateParams.top];
-        console.log(topic);
         var quiz = topic.quizzes[$stateParams.id];
-        console.log(quiz);
         this.title = quiz.title;
         this.description = quiz.description;
-        this.example = quiz.example;
+        this.tutorial = quiz.example;
         this.test = quiz.test;
-        this.calculator = quiz.calculator;
+        this.tool = quiz.tool;
+        this.current = undefined; // Can be undefined, tool, tutorial or test
     }
 }
 
--- src/client/app/components/table/table.js
+++ src/client/app/components/table/table.js
@@ -2,23 +2,18 @@ var exprParser = require('util/expression_parser.js');
 
 require('./table.scss');
 
-class TableCtrl {
-    constructor(table) {
-
-        this.table = table;
-
-        this.calculate = (expr) => exprParser.calculate(expr, this.data);
-    }
-}
-
 class TableDirective {
     constructor() {
         this.template = require('./table.html');
         this.restrict = 'E';
         this.replace = true;
+        this.scope = {
+            table : '=',
+            answer : '=',
+            calculate : (expr) => exprParser.calculate(expr, this.data)
+        }
     }
 }
 
 angular.module('app')
-    .controller('TableCtrl', TableCtrl)
     .directive('spreadsheet', () => new TableDirective);
