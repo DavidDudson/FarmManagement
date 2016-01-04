d66428dca454a16fee9a45141bc597f6a87edc7c
Anthony Crowcroft <anthonycrowcroft@Anthonys-iMac.local>
2016-01-05 10:18:33 +1300

    moved to a directive

--- src/client/app/components/nav/bottom/bottom.js
+++ src/client/app/components/nav/bottom/bottom.js
@@ -1,1 +1,9 @@
-// Todo: Implement this
+class BottomDirective {
+    constructor() {
+        this.template = require('./bottom.html');
+        this.restrict = 'E';
+    }
+}
+
+angular.module('app')
+    .directive('bottomNav', () => new BottomDirective());
