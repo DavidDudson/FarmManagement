24d019eb00f2c33ff2e97c345394e5ab35f217bb
David Dudson <d.dudson@psenterprise.com>
2016-01-04 20:47:36 +1300

    Only show actions while admin

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -25,6 +25,7 @@ class AppCtrl {
         this.yearOfCreation = 2015;
         this.editable = false;
         this.isMobile = false;
+        this.isAdmin = false;
         this.images = [require('images/Cows.jpg'),require('images/CowshedDude.jpg')];
         this.topics = require('./example.json').topics; // Todo Add Call to database which returns example Json if nothing exists
         $rootScope.app = this;
