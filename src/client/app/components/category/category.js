584d76eccca6478ce190f5ccd3ff4247ee0b81cc
David Dudson <d.dudson@psenterprise.com>
2016-01-10 21:57:42 +1300

    Finish topic route

--- src/client/app/components/category/category.js
+++ src/client/app/components/category/category.js
@@ -1,25 +1,26 @@
 var _ = require('lodash');
 
 class CategoryCtrl {
-    constructor($rootScope, topics ,$stateParams) {
+    constructor($rootScope, topics, $stateParams) {
         var category = _.find($rootScope.app.categories, {id: _.parseInt($stateParams.id)});
         this.title = category.title;
         this.id = category.id;
         this.description = category.description;
-        this.topics = topics
+        this.topics = topics;
+        $rootScope.app.topics = this.topics;
     }
 }
 
 angular.module('app')
     .config(($stateProvider) => {
         $stateProvider
-            .state("top" , {
-                url:'/category/{id}',
+            .state("top", {
+                url: '/category/{id}',
                 template: require('./category.html'),
                 controller: CategoryCtrl,
                 controllerAs: "category",
                 resolve: {
-                    topics : ($http, $stateParams) => $http.get("/category?id=" + $stateParams.id)
+                    topics: ($http, $stateParams) => $http.get("/category?id=" + $stateParams.id)
                         .then(res => res.data, err => console.log(err))
                 }
             })
--- src/client/app/components/topic/topic.js
+++ src/client/app/components/topic/topic.js
@@ -4,8 +4,10 @@ var _ = require('lodash');
 
 class TopicCtrl {
     constructor($rootScope, $stateParams) {
-        var category = _.find($rootScope.app.categories, {id: _.parseInt($stateParams.top)});
-        var topic =  _.find(category.Topics, {id: _.parseInt($stateParams.id)});
+        var topic =  _.find($rootScope.app.topics, {id: _.parseInt($stateParams.id)});
+        console.log($rootScope.app.topics);
+        console.log($stateParams.id);
+        console.log(topic);
         this.title = topic.title;
         this.description = topic.description;
         this.tutorial = topic.example;
@@ -19,9 +21,14 @@ angular.module('app')
     .config(($stateProvider) => {
         $stateProvider
             .state("topic" , {
-                url: '/topic/{top}/{id}',
+                url: '/topic/{id}',
                 template: require('./topic.html'),
                 controller: TopicCtrl,
-                controllerAs: "topic"
+                controllerAs: "topic",
+                resolve: {
+                    categoryCheck: ($rootScope, $location) => {
+                        if (!$rootScope.app.categories) $location.path('/home');
+                    }
+                }
             })
     });
