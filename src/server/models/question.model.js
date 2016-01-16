de4f064af86938d166d0bc4a024004e19554f1fa
Anthony Crowcroft <anthonycrowcroft@Anthonys-iMac.local>
2016-01-16 16:28:38 +1300

    finished schema with revision

--- src/server/models/question.model.js
+++ src/server/models/question.model.js
@@ -4,7 +4,7 @@ var mongoose    = require('mongoose');
 var questionSchema = new mongoose.Schema({
     title       : String,
     question    : String,
-    table       : JSON
+    table       : [[String]]
 });
 
 module.exports = mongoose.model('Question', questionSchema);
