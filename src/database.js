a82ee9a04ed0d0441d4c009065c96e561a8f7dcf
Anthony Crowcroft <anthonycrowcroft@Anthonys-iMac.local>
2016-01-19 09:44:15 +1300

    database import from json

--- src/database.js
+++ src/database.js
@@ -9,25 +9,29 @@ var Question = Promise.promisifyAll(require("./server/models/question.model"));
 var example = require("./example.json").categories;
 
 module.exports = function() {
-    //TODO implement this
     console.log("establish all data in example.json in database if requested");
     if(example && example.length != 0) {
         example.forEach(function (c) {
             var newC = _.cloneDeep(c);
+            newC.topics = [];
             if(c.topics && c.topics.length != 0) {
                 c.topics.forEach(function (t) {
                     var newT = _.cloneDeep(t);
+                    newT.questions = [];
                     if(t.questions && t.questions.length != 0) {
                         t.questions.forEach(function(q) {
-                            addToDB(Question, q);
-                            //build questions newObject with references to these objects as they are made
+                            var DBq = new Question(q);
+                            addToDB(Question, DBq);
+                            newT.questions.push(DBq);
                         });
                     }
-                    addToDB(Topic, newT);
-                    //build questions newObject with references to these objects as they are made
+                    var DBt = new Topic(newT);
+                    addToDB(Topic, DBt);
+                    newC.topics.push(newT);
                 });
             }
-            addToDB(Category, newC);
+            var DBc = new Category(newC);
+            addToDB(Category, DBc);
         });
     }
 };
@@ -36,10 +40,12 @@ var addToDB = function(schema, object) {
     //check if in DB already
     schema.findOne({"title": "object.title"}, function(err, result) {
         if(!result) {
-            //add to DB if not found
-            var newObj = new schema(object);
-            console.log(newObj.title + " added to " + "??correct??" + " table in mongoDB");
+            object.save(function (err) {
+                if (!err)
+                    console.log("saved in DB");
+            });
         }
+
     });
 
 };
