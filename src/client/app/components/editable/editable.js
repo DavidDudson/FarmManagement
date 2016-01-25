994a6c22e06260913b71445901979aff553242e4
David Dudson <d.dudson@psenterprise.com>
2016-01-25 22:43:43 +1300

    Implement editable WIP

---
+++ src/client/app/components/editable/editable.js
@@ -0,0 +1,44 @@
+require('./editable.scss');
+
+var ROOT = undefined;
+var SCOPE = undefined;
+
+class EditableController {
+
+    constructor($rootScope, $scope) {
+        ROOT = $rootScope;
+        SCOPE = $scope;
+    }
+
+    showButton() {
+        if (ROOT.app.editable) {
+            $scope.show = true;
+        }
+    };
+
+    hideButton() {
+        if (ROOT.app.editable) {
+            $scope.show = false;
+        }
+    };
+}
+
+class EditableDirective {
+    constructor() {
+        this.template = require('./editable.html');
+        this.restrict = 'E';
+        this.replace = true;
+        this.transclude = true;
+        this.controller = EditableController;
+        this.controllerAs = "editable";
+        this.scope = {
+            value: '=',
+            name: '=',
+            type: '=',
+            description: '='
+        }
+    }
+}
+
+angular.module('app')
+    .directive('editable', () => new EditableDirective());
