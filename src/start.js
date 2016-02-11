c6109f26b8a74aa351a57bd31b59543e296a7bb9
David Dudson <d.dudson@psenterprise.com>
2016-02-12 01:47:40 +1300

    Heroku logging of dirs

--- src/start.js
+++ src/start.js
@@ -1,6 +1,12 @@
 // Run the build
 require('./build.js')();
 
+var path = require('path');
+
+var homedir = path.resolve(__dirname + '/../');
+
+console.log(homedir);
+console.log(__dirname);
 var app = require('./server/server.js');
 
 port = process.env.PORT || 9000;
