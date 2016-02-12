5bd48b75c0f5b64edd14dd997bb0fe2d970c6380
David Dudson <d.dudson@psenterprise.com>
2016-02-12 19:32:24 +1300

    Add About page and uisref links

---
+++ src/client/app/components/about/about.js
@@ -0,0 +1,17 @@
+angular.module('app')
+    .config(($stateProvider) => {
+
+        $stateProvider
+            .state("about", {
+                url: '/about',
+                views: {
+                    '': {
+                        template: require('./about.html'),
+                    },
+                    'nav': require('components/nav/scroll/scroll.js')
+                },
+                resolve: {
+                    categories: ($http, $rootScope) => $http.get("/categories")
+                }
+            });
+    });
