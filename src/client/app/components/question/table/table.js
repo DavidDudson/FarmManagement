d2c66e889820cf02fbc60d8934423b8d7c15a4ce
David Dudson <d.dudson@psenterprise.com>
2016-02-25 00:05:11 +1300

    Implement Cyclic dependency management code

--- src/client/app/components/question/table/table.js
+++ src/client/app/components/question/table/table.js
@@ -11,46 +11,50 @@ class TableController {
     constructor($scope) {
         this.calculate = (expr) => exprParser.calculate(expr, this.table);
         this.rawTable = $scope.data.map(rowData => rowData.rowContent);
-        this.getVars = s => s.match(/(\[[A-Z][0-9]])/g);
-        this.getInitial = cell => (this.isToolInput(cell) || this.isTestInput()) ? 0 : cell;
+        this.getDependencies = s => {
+            var deps = s.match(/(\[[A-Z][0-9]])/g);
+            if (deps == null) {
+                return [];
+            } else {
+                return deps.map(d => d.replace(/(\[|])/g,""));
+            }
+        };
+
+
+        this.getType = s => {
+            if (this.isInput(s)) {
+                return "input";
+            } else if (this.isTutorialExample(s)) {
+                return "hover"
+            } else {
+                return "computed"
+            }
+        };
+        this.getInitial = cell => cell.type === "input" ? 0 : cell;
         this.parse = s => this.calculate(this.replaceVars(_.trim(s, "? ")));
-        this.convertFromIndex = (row,col) => [String.fromCharCode(row + 65), col + 1];
+        this.convertFromIndex = (row,col) => [String.fromCharCode(row + 65), col + 1].join("");
         this.convertToIndex = s => [this.convertLetterToIndex(s.match(/([A-Z])/)[0]), _.parseInt(s.match(/([0-9])/)[0]) - 1];
         this.convertLetterToIndex = s => s.charCodeAt(0) - 65;
-        this.replaceVars = s => s.replace(new RegExp(this.getVarMapp.map(m => _.escapeRegExp(m[0])).join("|"),"g"), m => table);
+        this.replaceVars = c => {
+            c.current.map(d => d.replace(/(\[|])/g,""));
+            c.dependencies.forEach(d => c.current.replace(d, _.find(this.table, {"index" : d})));
+        };
         this.isTutorialExample = s => $scope.mode === 'tutorial' && this.isSpecialCell(s);
         this.isTestInput = s => $scope.mode === 'test' && this.isSpecialCell(s);
         this.isToolInput = s => $scope.mode === 'tool' && !this.isSpecialCell(s);
         this.isSpecialCell = s => s.match(/^\?.*/);
-        this.isCalculateable = s => !this.isToolInput(s) && !this.isTestInput(s);
+        this.isInput = s => !this.isToolInput(s) && !this.isTestInput(s);
         this.computeTableValues = () => {
-            console.log("Computing table values");
-            var queue = _(this.table).flatten().sortBy(o => _.size(o.dependencies)).value();
-
-            console.log(queue);
-
-            while(queue.length > 0){
-                var o = queue.shift();
-                console.log("Got object:");
-                console.log(o);
-
-                var result = this.computeValue(o);
-                if (result === false) {
-                    console.log("Pushing Back onto queue: " + o.current);
-                        // queue.push(o);
-                } else {
-                    console.log("Adding to calculated: " + result);
-                    o.calculated = result;
-                }
-                console.log(queue);
-            }
+            _(this.table).flatten().filter(c => c.dependencies === []).forEach(c => this.computeValue(c));
+            console.log(this.sortedGraph);
+            //TODO
         };
         this.computeValue = o => {
             if (o.current.match(/^[A-Za-z ]+$/) !== null) {
                 console.log("Compute: Basic " + o.current);
                 return o.current
             } else {
-                var x = (o.dependencies === null || this.dependenciesExist(o)) ? this.parse(this.replaceVars(o)) : false;
+                var x = (o.dependencies === [] || this.dependenciesExist(o)) ? this.parse(this.replaceVars(o)) : false;
                 console.log("Compute: " + x);
                 console.log("Compute: " +  o.current);
                 return x;
@@ -60,22 +64,36 @@ class TableController {
             return this.convertToIndex[v.row][v.col].calculated != undefined;
         }));
         this.table = this.rawTable.map((row, i) => row.map((cell, j) => {
-            return {current: this.getInitial(cell), dependencies : this.getVars(cell), row : i, col : j};
+            return {
+                current: this.getInitial(cell),
+                dependencies : this.getDependencies(cell),
+                row : i,
+                col : j,
+                index : this.convertFromIndex(i, j),
+                type : this.getType(cell)
+            };
         }));
         this.getTopHeadings = () => $scope.top == true ? this.table[0] : undefined;
         this.sortTable = () => {
             var graph = tsort();
-            this.table
-                .filter(c => c.dependencies != null && c.dependencies != undefined)
+            _(this.table).flatten()
+                .filter(c => c.dependencies != [])
                 .forEach(cell => {
-                    var humanReadibleIndex = this.convertFromIndex(cell.row, cell.col);
-                    cell.dependencies.forEach(dep => graph.add(humanReadibleIndex, dep));
+                    console.log(cell);
+                    cell.dependencies.forEach(dep => {
+                        console.log([cell.index,dep]);
+                        graph.add(cell.index, dep);
+                    });
                 });
 
-            console.log(graph.sort());
+            console.log("TSORT GRAPH:");
+            console.log(graph);
+            graph.sort();
+            console.log("Post TSORT: ");
+            console.log(graph);
+            return graph;
         };
         this.sortedGraph = this.sortTable();
-
     }
 }
 
